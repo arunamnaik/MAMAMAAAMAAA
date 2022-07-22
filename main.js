3368520f146de1a05b88e7182e95d1403157f77e
arunamnaik <70759516+arunamnaik@users.noreply.github.com>
2022-07-22 07:20:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550,500);
+    canvas.position(600,150);
+    poseNet = ml5.poseNet(video, modalLoaded);
+    poseNet.on("pose",gotPoses);
+}
+
+function draw(){
+    document.getElementById("font_size").innerHTML= "Size of the font ="+difference+"px"
+    fill("#5c0600");
+    text("gcbuhsgfhmgexomuiqwhmui",noseX,noseY);
+    textSize(difference);
+
+}
+
+function modalLoaded(){
+    console.log("Posenet is initiallized");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX ="+noseX+"noseY ="+noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = Math.floor(leftWristX - rightWristX);
+        console.log("leftWristX ="+leftWristX+"rightWristX"+rightWristX+"difference"+difference);
+    }
+}
